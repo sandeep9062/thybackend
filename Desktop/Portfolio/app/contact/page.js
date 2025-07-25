7ed58e6d2270312f996c7aef959d11cede28bd94
Sandeep Saini <sandeepsaini157219@gmail.com>
2025-07-26 01:18:58 +0530

    Fix: Ensure proper Next.js setup for Vercel

---
+++ Desktop/Portfolio/app/contact/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      contact mee-files based routing
+
+    <h1 className='text-xl'>PROBLEM CREATED</h1>
+<ul>
+
+
+    <li> full stack solution</li>
+</ul>
+
+
+    </div>
+  )
+}
+
+export default contact
