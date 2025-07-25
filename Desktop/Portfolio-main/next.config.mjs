03db6c94dd6f054a41ca6bfd6b6a93b1b2c367f9
Sandeep Saini <sandeepsaini157219@gmail.com>
2025-07-26 00:41:58 +0530

    Fix: added missing pages or app directory

--- Desktop/Portfolio-main/next.config.mjs
+++ Desktop/Portfolio-main/next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,
+    },
+  }
+  
+  module.exports = nextConfig
+  
